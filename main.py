cb9a2ac1a71386d561f25a379aad8ddd0b9d5b4e
unknown <firstsonofsea@yandex.ru>
2020-11-01 01:36:14 +0300

    refactor

--- main.py
+++ main.py
@@ -9,15 +9,29 @@ class Runner(object):
         self.lang = lang
         self.username = username
         self.CURRENT_LANG = ['Python', 'C++', 'PASCAL']
+        self.CURRENT_TYPE_INPUT = ['FILE', 'STRING']
         self.CURRENT_PATH = os.getcwd()
         self.output = output
         self.input_str = input_str
         self.type_input = type_input
 
+    def create_main_file(self, file_name):
+        file = open(file_name, 'w')
+        file.write(self.code)
+        file.close()
+        return True
+
     def create_file(self, i):
         file = open('input.bat', 'w')
         file.write(self.input_str[i])
         file.close()
+        return True
+
+    def check_result(self, i, result, result_func):
+        if result.stdout.strip() == self.output[i]:
+            result_func['tests'].append('OK')
+        else:
+            result_func['tests'].append('ERROR')
         return True
 
     def run_compile(self):
@@ -32,34 +46,28 @@ class Runner(object):
         result_func = {'status': None, 'tests': []}
         file_name = self.username + r'.cpp'
         file_name_exe = self.username + r'.exe'
-        file = open(file_name, 'w')
-        file.write(self.code)
-        file.close()
+        self.create_main_file(file_name)
         compile_file = subprocess.run(['g++', file_name, '-o', file_name_exe],
                                       stdout=subprocess.PIPE,
                                       stderr=subprocess.PIPE)
         if compile_file.returncode == 0:
             result_func['status'] = 'OK'
             for i in range(len(self.input_str)):
-                if self.type_input == 'FILE':
+                if self.type_input == self.CURRENT_TYPE_INPUT[0]:
                     self.create_file(i)
                     result = subprocess.run([file_name_exe],
                                             stdout=subprocess.PIPE,
                                             text=True,
                                             stderr=subprocess.PIPE)
                     os.remove('input.bat')
-                elif self.type_input == 'STRING':
+                elif self.type_input == self.CURRENT_TYPE_INPUT[1]:
                     result = subprocess.run([file_name_exe],
                                             stdout=subprocess.PIPE,
                                             text=True,
                                             stderr=subprocess.PIPE,
                                             input=self.input_str[i])
                 if result.returncode == 0:
-                    if result.stdout.strip() == self.output[i]:
-                        result_func['tests'].append('OK')
-                    else:
-
-                        result_func['tests'].append('ERROR')
+                    self.check_result(i, result, result_func)
                 else:
                     result_func['status'] = 'ERROR'
                     result_func['tests'].append(result.stderr.strip())
@@ -74,18 +82,16 @@ class Runner(object):
     def run_python(self):
         result_func = {'status': None, 'tests': []}
         file_name = self.username + r'.py'
-        file = open(file_name, 'w')
-        file.write(self.code)
-        file.close()
+        self.create_main_file(file_name)
         for i in range(len(self.input_str)):
-            if self.type_input == 'FILE':
+            if self.type_input == self.CURRENT_TYPE_INPUT[0]:
                 self.create_file(i)
                 result = subprocess.run(['python', file_name],
                                         stdout=subprocess.PIPE,
                                         text=True,
                                         stderr=subprocess.PIPE)
                 os.remove('input.bat')
-            elif self.type_input == 'STRING':
+            elif self.type_input == self.CURRENT_TYPE_INPUT[1]:
                 result = subprocess.run(['python', file_name],
                                         stdout=subprocess.PIPE,
                                         text=True,
@@ -94,9 +100,7 @@ class Runner(object):
             if result.returncode == 0:
                 result_func['status'] = 'OK'
                 if result.stdout.strip() == self.output[i]:
-                    result_func['tests'].append('OK')
-                else:
-                    result_func['tests'].append('ERROR')
+                    self.check_result(i, result, result_func)
             else:
                 result_func['status'] = "ERROR"
                 result_func['tests'].append(result.stderr)
@@ -108,34 +112,28 @@ class Runner(object):
         result_func = {'status': None, 'tests': []}
         file_name = self.username + r'.pas'
         file_name_exe = self.username + r'.exe'
-        file = open(file_name, 'w')
-        file.write(self.code)
-        file.close()
+        self.create_main_file(file_name)
         compile_file = subprocess.run([r'C:\FPC\3.2.0\bin\i386-win32\ppc386.exe', file_name],
                                       stdout=subprocess.PIPE,
                                       stderr=subprocess.PIPE)
         if compile_file.returncode == 0:
             result_func['status'] = 'OK'
             for i in range(len(self.input_str)):
-                if self.type_input == 'FILE':
+                if self.type_input == self.CURRENT_TYPE_INPUT[0]:
                     self.create_file(i)
                     result = subprocess.run([file_name_exe],
                                             stdout=subprocess.PIPE,
                                             text=True,
                                             stderr=subprocess.PIPE)
                     os.remove('input.bat')
-                elif self.type_input == 'STRING':
+                elif self.type_input == self.CURRENT_TYPE_INPUT[1]:
                     result = subprocess.run([file_name_exe],
                                             stdout=subprocess.PIPE,
                                             text=True,
                                             stderr=subprocess.PIPE,
                                             input=self.input_str[i])
                 if result.returncode == 0:
-                    if result.stdout.strip() == self.output[i]:
-                        result_func['tests'].append('OK')
-                    else:
-
-                        result_func['tests'].append('ERROR')
+                    self.check_result(i, result, result_func)
                 else:
                     result_func['status'] = 'ERROR'
                     result_func['tests'].append(result.stderr.strip())
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from main import Runner
 
-#file = open('tupo_test.py', 'r')
-#text = file.read()
-#r = Runner(text, 'Python', 'mihailo', ['1', '3'], ['1 2 3 4 5', '5 4 3 15'])
-#print(r.run_compile())
+file = open('tupo_test.py', 'r')
+text = file.read()
+r = Runner(text, 'Python', 'mihailo', ['1', '3', '6'], ['1 2 3 4 5', '5 4 3 15', '5 3 2'], 'STRING')
+print(r.run_compile())
 
 file = open('paskal.txt', 'r')
 text = file.read()
 r = Runner(text, 'PASCAL', 'mihailo', ['1', '3','41'], ['0 1', '1 2','6 10'], 'STRING')
+print(r.run_compile())
+
+file = open('plpl.txt', 'r')
+text = file.read()
+r = Runner(text, 'C++', 'mihailo', ['3', '1','41'], ['5 3 4 9 0', '1 2 2 0', '6 10 12 3 0'], 'STRING')
 print(r.run_compile())
